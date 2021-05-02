8bb7bf3763ce77840ba72c6e19947858a2045a2d
andriansoelistiyo <andrian.socialmedia@gmail.com>
2021-05-02 15:46:00 +0700

    update home

--- pages/home/index.php
+++ pages/home/index.php
@@ -13,6 +13,7 @@
 
     <!-- css plugins -->
     <link rel="stylesheet" href="../../assets/plugins/bootstrap-4.0.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/plugins/fontawesome-free-5.15.2-web/css/all.min.css">
     <!-- ./css plugins -->
 
     <!-- css foundations -->
@@ -24,33 +25,97 @@
     <!-- ./css foundations -->
 
     <!-- css components -->
+    <link rel="stylesheet" href="../../assets/css/components/summary.css">
     <link rel="stylesheet" href="../../assets/css/components/amcharts.css">
     <!-- ./css components -->
 </head>
 
 <body class="layout-dashboard">
-    <div class="row">
-        <div class="col-4">
-            <div id="line_chart-1"></div>
-        </div>
-        <div class="col-4">
-            <div id="line_chart-2"></div>
-        </div>
-        <div class="col-4">
-            <div id="gauge-1"></div>
-        </div>
-    </div>
+    <!-- Image and text -->
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="../../assets/img/pertamina-logo.png" width="auto" height="30" class="d-inline-block align-top" alt="">
+        </a>
+    </nav>
 
-    <div class="row">
-        <div class="col-4">
-            <div id="line_chart-3"></div>
+    <div class="container-fluid">
+        <!-- first row -->
+        <div class="section mt-16">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header font-weight-bold fs-heading04">
+                            PRODUKSI MINYAK LIMAU April 2020 s/d April 2021 (BOPD)
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-3">
+                                    <p class="font-weight-bold fs-heading03">3,407.03 [<i class="fas fa-circle text-danger px-8"></i> 80.37%]</p>
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <p class="fs-body01">Status produksi</p>
+                                        </div>
+                                        <div class="col-6">
+                                            <p class="font-weight-bold fs-body01 mb-8">: 18 April 2021</p>
+                                            <p class="font-weight-bold fs-body01"><i class="fas fa-long-arrow-alt-down text-danger pr-8"></i> 80.37%</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div id="line_chart-1"></div>
+                                </div>
+                                <div class="col-3">
+                                    <div id="line_chart-2"></div>
+                                </div>
+                                <div class="col-3">
+                                    <div id="gauge-1"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-4">
-            <div id="line_chart-4"></div>
+        <!-- ./first row -->
+
+        <!-- second row -->
+        <div class="section mt-32">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header font-weight-bold fs-heading04">
+                            PRODUKSI GAS LIMAU April 2020 s/d April 2021 (BOPD)
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-3">
+                                    <p class="font-weight-bold fs-heading03">3,407.03 [<i class="fas fa-circle text-success px-8"></i> 80.37%]</p>
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <p class="fs-body01">Status produksi</p>
+                                        </div>
+                                        <div class="col-6">
+                                            <p class="font-weight-bold fs-body01 mb-8">: 18 April 2021</p>
+                                            <p class="font-weight-bold fs-body01"><i class="fas fa-long-arrow-alt-up text-success pr-8"></i> 80.37%</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div id="line_chart-3"></div>
+                                </div>
+                                <div class="col-3">
+                                    <div id="line_chart-4"></div>
+                                </div>
+                                <div class="col-3">
+                                    <div id="gauge-2"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-4">
-            <div id="gauge-2"></div>
-        </div>
+        <!-- ./second row -->
     </div>
 
     <!-- library bootstrap script -->
@@ -59,11 +124,11 @@
     <script src="../../assets/plugins/bootstrap-4.0.0-dist/js/bootstrap.bundle.min.js" type="text/javascript" charset="utf-8"></script>
     <!-- ./library bootstrap script -->
 
-    <!-- library amcharts script -->
-    <script src="https://cdn.amcharts.com/lib/4/core.js"></script>
-    <script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
-    <script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
-    <!-- ./library amcharts script -->
+    <!-- library CDN amcharts script -->
+    <script src="https://cdn.amcharts.com/lib/4/core.js" type="text/javascript"></script>
+    <script src="https://cdn.amcharts.com/lib/4/charts.js" type="text/javascript"></script>
+    <script src="https://cdn.amcharts.com/lib/4/themes/animated.js" type="text/javascript"></script>
+    <!-- ./library CDN amcharts script -->
 
     <!-- list amcharts -->
     <script src="../../assets/js/amcharts/line_chart-1.js" type="text/javascript" charset="utf-8"></script>
@@ -73,21 +138,6 @@
     <script src="../../assets/js/amcharts/gauge-1.js" type="text/javascript" charset="utf-8"></script>
     <script src="../../assets/js/amcharts/gauge-2.js" type="text/javascript" charset="utf-8"></script>
     <!-- ./list amcharts -->
-
-    <!-- custom jquery script -->
-    <script type="text/javascript">
-        $(document).on('ready', function() {
-            $(".center").slick({
-                dots: true,
-                arrows: true,
-                infinite: true,
-                centerMode: true,
-                slidesToShow: 1,
-                slidesToScroll: 1
-            });
-        });
-    </script>
-    <!-- ./custom jquery script -->
 </body>
 
 </html>
