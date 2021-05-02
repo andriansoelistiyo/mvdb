a4a2005ff228fc20a0d5384fac98fa210ea15ca8
andriansoelistiyo <andrian.socialmedia@gmail.com>
2021-05-02 15:59:27 +0700

    update home

--- pages/home/index.php
+++ pages/home/index.php
@@ -34,7 +34,7 @@
     <!-- Image and text -->
     <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">
-            <img src="../../assets/img/pertamina-logo.png" width="auto" height="30" class="d-inline-block align-top" alt="">
+            <img src="../../assets/img/pertamina-logo.png" width="auto" height="30" class="d-inline-block align-top" alt="pertamina logo">
         </a>
     </nav>
 
