f3721fce5cccb1a0001aaf8959744b064129fb9c
andriansoelistiyo <andrian.socialmedia@gmail.com>
2021-05-05 16:09:52 +0700

    update chart

--- assets/js/amcharts/custom_themes.js
+++ assets/js/amcharts/custom_themes.js
@@ -1,6 +1,6 @@
 function am4themes_green(target) {
   if (target instanceof am4core.ColorSet) {
-    target.list = [am4core.color("#1BA68D")];
+    target.list = [am4core.color("#39FF14")];
   }
 }
 function am4themes_red(target) {
